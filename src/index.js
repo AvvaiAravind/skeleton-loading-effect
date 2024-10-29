359927fffe0062460a41455c9e58d35895e6fc81
Avvai Aravind <avvaiaravind@gmail.com>
2024-10-29 17:06:54 +0530

    created skeleton effect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./style.css"; /*  for importing style.css */
+
+window.onload = () => {
+  const array = document.querySelectorAll(".skeleton");
+  setTimeout(() => {
+    array.forEach((arr) => arr.classList.remove("skeleton"));
+  }, 5000);
+};
